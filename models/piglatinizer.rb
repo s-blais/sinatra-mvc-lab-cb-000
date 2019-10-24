a951f02c470c5461390a6a7ed82e328284e86c04
Stephen Blais <stephen@s-blais.com>
2019-10-24 19:21:58 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,21 @@
+class PigLatinizer
+
+  # IMHO, this is better than the posted solution. All in one method, one word or multiple, doesn't care. Also doesn't care how many consonants the word starts with. Why write specific code for each possibility of one or two or three consonants? Ugh. What about schnozz? schlock? sphygmomanometer? My code works with those!
+
+  def piglatinize(user_phrase)
+    word_array = user_phrase.split(" ").collect do |word|
+      word_letters = word.split("")
+      leading_consonants = ""
+      if word.start_with?(/[aeiouAEIOU]/)
+        word += "way"
+      else
+        while !word_letters[0].match(/[aeiouAEIOU]/)
+          leading_consonants << word_letters.shift
+          end
+        word = word_letters.join("") + leading_consonants + "ay"
+      end
+    end
+    word_array.join(" ")
+  end
+
+end
